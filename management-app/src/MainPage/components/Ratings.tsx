283a25b41f652571cf22de5e89b241a17b475954
Nityaa1802 <nitya.agarwal_cs.aiml20@gla.ac.in>
2023-07-23 00:17:32 +0530

    added rating of the product to table row

--- management-app/src/MainPage/components/Ratings.tsx
+++ management-app/src/MainPage/components/Ratings.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-const Ratings = () => {
+const Ratings = (props: any) => {
+  var ratingCopy = props.rating;
+  const rating = [];
+  while (ratingCopy > 0.1) {
+    if (ratingCopy > 1) {
+      rating.push(faStar);
+    } else {
+      rating.push(faStarHalf);
+    }
+    ratingCopy--;
+  }
   return (
-      <div>
-          
-  </div>
-  )
-}
+    <div className="justify-start">
+      {rating.map((rate) => {
+        return <FontAwesomeIcon icon={rate} style={{ color: "#ffc800" }} />;
+      })}
+    </div>
+  );
+};
 
-export default Ratings
+export default Ratings;
